484bf0b5698ed96464ed0968bd3cc6656707dcdb
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-09-25 14:41:33 +0200

    Ajout Blog Blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from . import auth
 from . import db
+from . import blog
 
 
 def create_app(test_config=None):
@@ -11,7 +12,8 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
-
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
